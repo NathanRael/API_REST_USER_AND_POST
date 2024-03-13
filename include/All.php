a9c04ec2834e94f442d24974e2c9b1e1ec73a315
NathanRael <ralaivoavy.natanael@gmail.com>
2024-03-13 16:31:15 +0300

    userReq

--- include/All.php
+++ include/All.php
@@ -5,6 +5,7 @@ require  __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
 require  __DIR__ .  DIRECTORY_SEPARATOR . 'UserController.php';
 require  __DIR__ .  DIRECTORY_SEPARATOR . 'EventController.php';
 require  __DIR__ .  DIRECTORY_SEPARATOR . 'SavedEventController.php';
+require  __DIR__ .  DIRECTORY_SEPARATOR . 'userRequestController.php';
 
 $conn = new DB();
 $pdo = $conn->connect();
